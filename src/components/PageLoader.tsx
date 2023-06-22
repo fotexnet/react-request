770febd311c4123d7eb37a39245b1843492a3985
Nagy Viktor <nagy.viktor@fotex.net>
2023-06-22 08:55:55 +0200

    fix: fixed styles

--- src/components/PageLoader.tsx
+++ src/components/PageLoader.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ScrollLock from 'react-scrolllock';
+import styles from '../styles/PageLoader.module.css';
 import Spinner, { SpinnerProps } from './Spinner';
 
 function PageLoaderComponent(props: SpinnerProps): JSX.Element {
   return (
     <ScrollLock>
-      <div className="absolute z-50 top-0 right-0 left-0 bottom-0 opacity-80 bg-white flex items-center justify-center">
+      <div className={styles.container}>
         <Spinner {...props} />
       </div>
     </ScrollLock>
