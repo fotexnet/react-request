14dbcba1d069c9288c49304706ea0f6eb6752784
Nagy Viktor <nagy.viktor@fotex.net>
2023-06-22 12:29:18 +0200

    fix: fixed page loader container

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { DEFAULT_COLOR } from '../constants';
 import styles from '../styles/Spinner.module.css';
 
 export type SpinnerProps = {
-  color?: string;
+  color?: string | [string, string, string];
   stripeWidth?: number;
   width?: number;
 };
 
 function Spinner({ color = DEFAULT_COLOR, width = 300, stripeWidth = 15 }: SpinnerProps): JSX.Element {
+  const firstColor = useMemo(() => (Array.isArray(color) ? color[0] : color), [color]);
+  const secondColor = useMemo(() => (Array.isArray(color) ? color[1] : color), [color]);
+  const thirdColor = useMemo(() => (Array.isArray(color) ? color[2] : color), [color]);
+
   return (
     <div className={styles.spinner__container} style={{ width, height: width }}>
       <div
         className={`${styles.spinner__sector} ${styles.spinner__sector_1}`}
-        style={{ borderTopColor: color, borderWidth: stripeWidth }}
+        style={{ borderTopColor: firstColor, borderWidth: stripeWidth }}
       ></div>
       <div
         className={`${styles.spinner__sector} ${styles.spinner__sector_2}`}
-        style={{ borderTopColor: color, borderWidth: stripeWidth }}
+        style={{ borderTopColor: secondColor, borderWidth: stripeWidth }}
       ></div>
       <div
         className={`${styles.spinner__sector} ${styles.spinner__sector_3}`}
-        style={{ borderTopColor: color, borderWidth: stripeWidth }}
+        style={{ borderTopColor: thirdColor, borderWidth: stripeWidth }}
       ></div>
     </div>
   );
